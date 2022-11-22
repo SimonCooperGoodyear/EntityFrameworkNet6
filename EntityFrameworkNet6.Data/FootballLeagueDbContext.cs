5584634769a6a3f4b5f3132815a6434a252c2ec4
SimonCooperGoodyear <sbull@coopertire.com>
2022-11-22 14:28:18 +0000

    Add project files.

---
+++ EntityFrameworkNet6.Data/FootballLeagueDbContext.cs
@@ -0,0 +1,18 @@
+﻿using EntityFrameworkNet6.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace EntityFrameworkNet6.Data
+{
+    public class FootballLeagueDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDb;Initial Catalog=FootballLeagueEFCore6")
+                .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Name }, LogLevel.Information);
+        }
+
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<League> Leagues { get; set; }
+    }
+}
---
+++ EntityFrameworkNet6.Domain/League.cs
@@ -0,0 +1,8 @@
+﻿namespace EntityFrameworkNet6.Domain
+{
+    public class League
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
